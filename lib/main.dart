9e61083100f0532048c136d1e445e437d8aff6ea
Saulo Gatti <saulogatti@gmail.com>
2026-04-02 18:23:49 -0300

    remover parâmetro de contexto do método removeOverlayAppView

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
           ),
           IconButton(
             onPressed: () {
-              OverlayAppViewManager.removeOverlayAppView(context);
+              OverlayAppViewManager.removeOverlayAppView();
             },
             icon: const Icon(Icons.remove),
           ),
--- lib/src/view/overlay_app_view_manager.dart
+++ lib/src/view/overlay_app_view_manager.dart
@@ -21,7 +21,7 @@ class OverlayAppViewManager {
     double height,
   ) {
     if (overlayEntry != null) {
-      removeOverlayAppView(context);
+      removeOverlayAppView();
     }
 
     if (width == 0 || height == 0) {
